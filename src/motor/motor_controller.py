e06a3e43e9c55dc372d052b1420cc3325142dd8d
arsnm <arsene.mallet2004@gmail.com>
2025-01-13 15:44:26 +0100

    debug

--- src/motor/motor_controller.py
+++ src/motor/motor_controller.py
@@ -53,6 +53,8 @@ class MotorController:
             item = self.command_queue.get()
             if item != ():
                 command, args = item
+                # log
+                print(args, type(args))
                 if self.terminate_all_event.is_set():
                     print("Terminate event is set, finishing...")
                     self.command_queue.task_done()
