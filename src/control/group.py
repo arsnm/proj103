46650a57c23c6cf82dd838f467927a8404a4e236
arsnm <arsene.mallet2004@gmail.com>
2025-01-23 11:23:33 +0100

    fixing group strat

--- src/control/group.py
+++ src/control/group.py
@@ -91,8 +91,8 @@ class GroupController:
                 time.sleep(self.check_interval)
 
     def _send_check(self):
-        headers = {"id": str(self.id - 1)}
-        response = requests.post(self.server_url, headers=headers, timeout=5)
+        id = str(self.id - 1)
+        response = requests.post(f"self.server_url/api/check?id={id}")
 
         if response.status_code == StrategyConfig.RESPONSE_MOVEMENT.value:
             try:
